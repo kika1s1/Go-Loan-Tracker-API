e137b1a437fd7f0a4a623c45f1ee0389a86ca8eb
kika1s1 <tamiratkebede120@gmail.com>
2024-08-27 16:32:06 +0300

    loan usecase is added

--- internal/usecase/loan/loan_usecase.go
+++ internal/usecase/loan/loan_usecase.go
@@ -1,1 +1,43 @@
 package loan
+
+import (
+	"github.com/kika1s1/Go-Loan-Tracker-API/internal/domain"
+	"github.com/kika1s1/Go-Loan-Tracker-API/internal/repository"
+)
+
+type LoanUseCase struct {
+	repo repository.LoanRepository
+}
+
+func NewLoanUseCase(repo *repository.LoanRepository) *LoanUseCase {
+	return &LoanUseCase{repo: *repo}
+}
+
+func (uc *LoanUseCase) ApplyForLoan(loan domain.Loan) (*domain.Loan, error) {
+	_, err := uc.repo.CreateLoan(loan)
+	if err != nil {
+		return nil, err
+	}
+	return &loan, nil
+}
+
+func (uc *LoanUseCase) ViewLoanStatus(id string) (*domain.Loan, error) {
+	return uc.repo.FindLoanByID(id)
+}
+
+func (uc *LoanUseCase) ViewAllLoans(status string, order string) ([]domain.Loan, error) {
+	return uc.repo.FindAllLoans(status, order)
+}
+
+func (uc *LoanUseCase) ApproveRejectLoan(id string, status string) (*domain.Loan, error) {
+	_, err := uc.repo.UpdateLoanStatus(id, status)
+	if err != nil {
+		return nil, err
+	}
+	return uc.repo.FindLoanByID(id)
+}
+
+func (uc *LoanUseCase) DeleteLoan(id string) error {
+	_, err := uc.repo.DeleteLoan(id)
+	return err
+}
