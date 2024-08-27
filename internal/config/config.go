f06d983af670d8540ffe007db2c88cf6d4f40995
kika1s1 <tamiratkebede120@gmail.com>
2024-08-27 14:32:31 +0300

    google and env configuration is added

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,29 +1,44 @@
 package config
 
 import (
-    "log"
-    "os"
-    "github.com/joho/godotenv"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-    ServerAddress string
-    DBUri         string
-    JWTSecret     string
+	MONGO_URI                   string
+	EMAIL_PROVIDER              string
+	SMTP_HOST                   string
+	EMAIL_PORT                  string
+	EMAIL_SENDER_EMAIL          string
+	EMAIL_SENDER_PASSWORD       string
+	APP_DOMAIN                  string
+	AI_API_DOMAIN               string
+	GO_PORT                     string
+	ACCESS_TOKEN_LIFE_TIME      string
+	REFRESH_TOKEN_LIFE_TIME     string
+	VERIFICATION_TOKEN_LIFETIME string
 }
 
-func LoadConfig() (*Config, error) {
-    err := godotenv.Load(".env")
-    if err != nil {
-        log.Fatalf("Error loading .env file: %v", err)
-        return nil, err
-    }
-
-    cfg := &Config{
-        ServerAddress: os.Getenv("SERVER_ADDRESS"),
-        DBUri:         os.Getenv("DB_URI"),
-        JWTSecret:     os.Getenv("JWT_SECRET"),
-    }
-
-    return cfg, nil
+func Load() (*Config, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return nil, err
+	}
+	Config := &Config{
+		MONGO_URI:                   os.Getenv("MONGO_URI"),
+		EMAIL_PROVIDER:              os.Getenv("EMAIL_PROVIDER"),
+		SMTP_HOST:                   os.Getenv("SMTP_HOST"),
+		EMAIL_PORT:                  os.Getenv("EMAIL_PORT"),
+		EMAIL_SENDER_EMAIL:          os.Getenv("SENDER_EMAIL"),
+		EMAIL_SENDER_PASSWORD:       os.Getenv("SENDER_PASSWORD"),
+		APP_DOMAIN:                  os.Getenv("APP_DOMAIN"),
+		GO_PORT:                     os.Getenv("GO_PORT"),
+		AI_API_DOMAIN:               os.Getenv("AI_API_DOMAIN"),
+		ACCESS_TOKEN_LIFE_TIME:      os.Getenv("ACCESS_TOKEN_LIFE_TIME"),
+		REFRESH_TOKEN_LIFE_TIME:     os.Getenv("REFRESH_TOKEN_LIFE_TIME"),
+		VERIFICATION_TOKEN_LIFETIME: os.Getenv("VERIFICATION_TOKEN_LIFETIME"),
+	}
+	return Config, nil
 }
