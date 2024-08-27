a62c45d7fdf5ad6fea7ef6aaca3d0d9480d834c1
kika1s1 <tamiratkebede120@gmail.com>
2024-08-27 10:34:15 +0300

    adding Environmental variable loading function

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,29 @@
 package config
+
+import (
+    "log"
+    "os"
+    "github.com/joho/godotenv"
+)
+
+type Config struct {
+    ServerAddress string
+    DBUri         string
+    JWTSecret     string
+}
+
+func LoadConfig() (*Config, error) {
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+        return nil, err
+    }
+
+    cfg := &Config{
+        ServerAddress: os.Getenv("SERVER_ADDRESS"),
+        DBUri:         os.Getenv("DB_URI"),
+        JWTSecret:     os.Getenv("JWT_SECRET"),
+    }
+
+    return cfg, nil
+}
