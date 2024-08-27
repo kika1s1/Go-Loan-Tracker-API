80bfdc3c668c91ad67352cee04b0f096ddbe5a2f
kika1s1 <tamiratkebede120@gmail.com>
2024-08-27 15:10:09 +0300

    refresh token and logout is added

--- internal/http/routes/auth_routes.go
+++ internal/http/routes/auth_routes.go
@@ -1,28 +1,28 @@
 package routes
 
 import (
-
 	"github.com/gin-gonic/gin"
+	"github.com/kika1s1/Go-Loan-Tracker-API/internal/http/handlers/token"
 )
 
 func RegisterVerificationRoutes(router *gin.Engine) {
 
 	userHandler := InstantaiteUserHandler()
-	authRoutes := router.Group("/api/v1/user")
+	authRoutes := router.Group("/api/v1/users")
 
 	{
 		authRoutes.POST("/login", userHandler.Login)
 		authRoutes.POST("/register", userHandler.Register)
 		authRoutes.GET("/google_redirect", userHandler.GoogleCallback)
-		authRoutes.POST("/verify/request", userHandler.RequestVerifyEmail)
+		authRoutes.POST("/verify-email", userHandler.RequestVerifyEmail)
 		authRoutes.GET("/verify/confirm", userHandler.VerifyEmail) //I used this naming to make things clear
 		authRoutes.POST("/reset-password/request", userHandler.ResetPasswordRequest)
 		authRoutes.POST("/reset-password/confirm", userHandler.ResetPassword)
 
-		
-
-		// r2 := authRoutes.Group("/")
-		// r2.POST("/logout", TokenHandler.LogOut)
-		// r2.POST("/refresh", TokenHandler.RefreshToken)
+		//logout and refresh
+		tokenHandler := &token.TokenHandler{}
+		r2 := authRoutes.Group("/")
+		r2.POST("/logout", tokenHandler.LogOut)
+		r2.POST("/token/refresh", tokenHandler.RefreshToken)
 	}
 }
