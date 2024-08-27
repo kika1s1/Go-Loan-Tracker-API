f96cc7b412d5fc53163b2a107ff78abf86ee64fc
kika1s1 <tamiratkebede120@gmail.com>
2024-08-27 11:47:14 +0300

    main is added

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	// "blogApp/internal/config"
+	// "blogApp/internal/http/routes"
+	// "blogApp/pkg/mongo"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kika1s1/Go-Loan-Tracker-API/internal/config"
+	"github.com/kika1s1/Go-Loan-Tracker-API/internal/http/routes"
+	"github.com/kika1s1/Go-Loan-Tracker-API/pkg/mongo"
+)
+
+func main() {
+	// os.Clearenv()
+	confs, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+	mongo.ConnectDB(confs.MONGO_URI)
+	defer mongo.DisconnectDB()
+	mongo.InitializeCollections()
+
+	router := gin.Default()
+	// Serve static files from the "upload" directory
+	routes.SetUpRoute(router)
+	router.Run(":" + confs.GO_PORT)
+}
