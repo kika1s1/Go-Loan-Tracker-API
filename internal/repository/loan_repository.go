dc80ee5101e042f422dc329acc0c02fb4eadb18e
kika1s1 <tamiratkebede120@gmail.com>
2024-08-27 16:33:11 +0300

    loan_repository interfece is added

---
+++ internal/repository/loan_repository.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"github.com/kika1s1/Go-Loan-Tracker-API/internal/domain"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type LoanRepository interface {
+	CreateLoan(loan domain.Loan) (*mongo.InsertOneResult, error)
+	FindLoanByID(id string) (*domain.Loan, error)
+	FindAllLoans(status string, order string) ([]domain.Loan, error)
+	UpdateLoanStatus(id string, status string) (*mongo.UpdateResult, error)
+	DeleteLoan(id string) (*mongo.DeleteResult, error)
+}
