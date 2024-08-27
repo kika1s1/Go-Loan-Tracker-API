54bcedabe129059b10d2c844e029f656aa479548
kika1s1 <tamiratkebede120@gmail.com>
2024-08-27 15:44:38 +0300

    loan and token strcut is added

--- internal/domain/laon.go
+++ internal/domain/laon.go
@@ -1,12 +1,9 @@
 package domain
 
-import "time"
 
 type Loan struct {
-    ID         string    `bson:"_id,omitempty" json:"_id,omitempty"`
-    UserID     string    `bson:"user_id" json:"user_id"`
-    Amount     float64   `bson:"amount" json:"amount"`
-    Status     string    `bson:"status" json:"status"`
-    CreatedAt  time.Time `bson:"created_at" json:"created_at"`
-    UpdatedAt  time.Time `bson:"updated_at" json:"updated_at"`
+    ID     string  `json:"id"`
+    Amount float64 `json:"amount"`
+    Term   int     `json:"term"`
+    Status string  `json:"status"`
 }
