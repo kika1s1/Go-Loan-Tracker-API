0cad5dedaff37d12458fd1aa500684bbf0a2a46a
kika1s1 <tamiratkebede120@gmail.com>
2024-08-27 10:18:57 +0300

    loan and user structure is created

--- internal/domain/laon.go
+++ internal/domain/laon.go
@@ -1,1 +1,12 @@
 package domain
+
+import "time"
+
+type Loan struct {
+    ID         string    `bson:"_id,omitempty" json:"_id,omitempty"`
+    UserID     string    `bson:"user_id" json:"user_id"`
+    Amount     float64   `bson:"amount" json:"amount"`
+    Status     string    `bson:"status" json:"status"`
+    CreatedAt  time.Time `bson:"created_at" json:"created_at"`
+    UpdatedAt  time.Time `bson:"updated_at" json:"updated_at"`
+}
--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,1 +1,11 @@
 package domain
+import "time"
+
+type User struct {
+	ID        string    `bson:"_id,omitempty" json:"id"`
+	Email     string    `bson:"email" json:"email"`
+	Password  string    `bson:"password" json:"password"`
+	IsAdmin   bool      `bson:"is_admin" json:"is_admin"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
